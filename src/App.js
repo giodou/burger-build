ecaf01406a53d462bf71e5494c95cb2b8917015d
Giovane <giovane.negocios@gmail.com>
2018-08-26 14:28:36 -0300

    enabled CSS modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className={classes.App}>
       </div>
     );
   }
